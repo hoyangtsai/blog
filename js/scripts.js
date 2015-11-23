ff9e2d7bfa12538217d10e0e3458f0274b58fec1
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-11-23 22:36:00 +0800

    backup script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,17 @@ var home = {
   init: function() {
     var click = 'touchstart' in window ? 'touchstart' : 'click';
     window.addEventListener(click, this.collapseNav);
+
+    $('.three-slide').on('webkitAnimationEnd', this.handleTransition);
+    $('.three-slide').on('animationend', this.handleTransition);
+
   },
-  collapseNav: function() {
+  collapseNav: function(e) {
+    e.preventDefault();
     $('#navbar').collapse('hide');
+  },
+  handleAnimation: function(e) {
+    console.log('Animation end');
   }
 };
 
