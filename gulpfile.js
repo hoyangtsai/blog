1522b1752f732138e84cf98c489fbc01f0260f45
Ho-Yang Tsai <hoyang.t@gmail.com>
2017-01-16 15:58:36 +0800

    Rid of source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('mincss', function() {
   return gulp.src('css/style.css')
-    .pipe(sourcemaps.init())
     .pipe(cleanCSS())
-    .pipe(sourcemaps.write())
     .pipe(rename({
       suffix: '.min'
     }))
