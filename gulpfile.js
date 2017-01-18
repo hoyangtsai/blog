650ef1a87139435c137ad937998b1f6aaf3d5ba5
Ho-Yang Tsai <hoyang.t@gmail.com>
2017-01-18 12:40:30 +0800

    Use sass and postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 var rename = require("gulp-rename");
 
 gulp.task('mincss', function() {
-  return gulp.src('css/style.css')
-    .pipe(cleanCSS())
+  var plugins = [
+    autoprefixer({browsers: ['> 5%']}),
+    cssnano()
+  ];
+  return gulp.src('css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
     .pipe(rename({
       suffix: '.min'
     }))
